eaf3fa681cc708db8aa38a79fbeb83bdb34546f4
Scott Vehrenkamp <scott.vehrenkamp@target.com>
2017-04-26 18:58:00 -0500

    adding redirect to server

--- app.js
+++ app.js
@@ -5,12 +5,8 @@ const PORT = process.env.PORT || 3000
 
 app.use(express.static('public'))
 
-app.get('/hoisting', (req,res) => {
-  res.sendFile(path.join(__dirname, 'public/hoisting.html'));
-})
-
-app.get('/objects', (req,res) => {
-  res.sendFile(path.join(__dirname, 'public/objects.html'));
-})
+app.get('/', (req,res) => res.sendFile(path.join(__dirname, 'public/objects.html')) )
+app.get('/hoisting', (req,res) => res.sendFile(path.join(__dirname, 'public/hoisting.html')) )
+app.get('/objects', (req,res) => res.sendFile(path.join(__dirname, 'public/objects.html')) )
 
 app.listen(PORT, () => console.log("server listening on %s", PORT) )
